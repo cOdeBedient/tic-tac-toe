f24330a9002af418b89ada4bafbc3573641cf531
Adam Bedient <adambedient@gmail.com>
2024-01-10 12:27:18 -0500

    Flesh out data model further

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ p1Wins = document.querySelector('#p1-wins');
 p2Wins = document.querySelector('#p2-wins');
 statusTitle = document.querySelector('.game-status');
 
+
 board.addEventListener('click', function(event) {
     var currentID = event.target.closest('id');
     manageBoardClick(currentID);
@@ -14,16 +15,36 @@ var player1 = {
     id: 'Toto',
     token : 'X',
     wins : 0,
-    currentSquares: []
+    currentSquares: {
+        aSquares: [],
+        bSquares: [],
+        cSquares: [],
+        oneSquares: [],
+        twoSquares: [],
+        threeSquares: [],
+        diagLRSquares: [],
+        diagRLSquares: []
+    }
 }
 
 var player2 = {
     id: 'Wicked Witch',
     token: 'O',
     wins: 0,
-    currentSquares: []
+    currentSquares: {
+        aSquares: [],
+        bSquares: [],
+        cSquares: [],
+        oneSquares: [],
+        twoSquares: [],
+        threeSquares: [],
+        diagLRSquares: [],
+        diagRLSquares: []
+    }
 }
 
+availableSquares = ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3'];
+whosTurn = 1;
 
 function manageBoardClick(iD) {
 }
